5db04ca3dd7b15409bcfe3c03e3c3149d8da96bd
AnJaWr <october3@o2.pl>
2019-09-06 22:50:07 +0200

    Add header arrow

--- script/main.js
+++ script/main.js
@@ -21,11 +21,13 @@ $(document).ready(function () {
         mhtml += '<h5>' + json.person[0].profession + '</h1>';
         mhtml += '</div>';
 
-        var $div = $('<div class="header__slider" style="background:url(' + json.person[0].picture + ');background-size:contain">').append($(mhtml));
+
+        var $btn = $('<button class="header__button">VIEW PROFILE</button><div class="header__arrow"><i class="fas fa-play"></i></div>');
+        var $div = $('<div class="header__slider" style="background:url(' + json.person[0].picture + ');background-size:contain;background-repeat:no-repeat;">').append($(mhtml));
         var $vert = $('<div class="haeder__line"><h3 class="header__number">' + json.person[0].index + '</h3></div><hr>');
         $('.header__center').append($div);
         $('.header__horizontal').append($vert);
-
+        $('.header__prev').append($btn);
         $('li.header__navlink').on("click", function () {
             key = $(this).val()
             $(this).addClass("active");
@@ -38,33 +40,14 @@ $(document).ready(function () {
             for (var i = 0; i < json.person.length; i++) {
                 if (json.person[i].index == key) {
 
-                    // mhtml = '<div class="header__title">';
-                    // mhtml += '<h1>' + json.person[i].firstname + '</h1>';
-                    // mhtml += '<h1>' + json.person[i].surname + '</h1>';
-                    // mhtml += '<h5>' + json.person[i].profession + '</h1>';
-                    // mhtml += '</div>';
 
-                    var $divnew = $('<div class="header__slider" style="background:url(' + json.person[i].picture + ');background-size:contain">').html($('<div class="header__title"><h1>' + json.person[i].firstname + '</h1><h1>' + json.person[i].surname + '</h1><h5>' + json.person[i].profession + '</h5></div>'));
+                    var $btnnew = $('<button class="header__button">VIEW PROFILE</button><div class="header__arrow"><i class="fas fa-play"></i></div>');
+                    var $divnew = $('<div class="header__slider" style="background:url(' + json.person[i].picture + ');background-size:contain; background-repeat:no-repeat">').html($('<div class="header__title"><h1>' + json.person[i].firstname + '</h1><h1>' + json.person[i].surname + '</h1><h5>' + json.person[i].profession + '</h5></div>'));
                     var $vertnew = $('<div class="haeder__line"><h3 class="header__number">' + json.person[i].index + '</h3></div><hr>');
                     $('.header__center').html($divnew);
-                    $('.header__horizontal').html($vertnew)
-                    // $('.header__slider').css({
-                    //     "width": "800px",
-                    //     "height": "600px",
-                    //     "background-repeat": "no-repeat",
-                    //     "background-size": "cover"
-                    // });
-                    // $('.header__title').css({
-                    //     "width": "200px",
-                    //     "height": "100px",
-                    //     "background": "rgb(179, 211, 211)",
-                    //     "position": "relative",
-                    //     "top": "200px",
-                    //     "left": "0"
-                    // })
+                    $('.header__horizontal').html($vertnew);
+                    $('.header__prev').html($btnnew);
 
-
-                    // );
 
                 }
 
@@ -73,21 +56,8 @@ $(document).ready(function () {
             $(this).siblings('li.active').removeClass("active");
             $(this > 'hr').sibilings('hr.active').removeClass("active");
 
-            // $('li.header__navlink').on("click", function () {
-            //     // debugger
-            //     $(this).addClass("active");
-            //     $(this > 'hr').addClass("active");
-            //     $(this).siblings().removeClass("active");
-            //     $(this > 'hr').sibilings().removeClass("active");
-            //     return key = $(this).val()
-
-
-            // });
 
         });
     })
-
-    // this.getData();
-
 
 });
